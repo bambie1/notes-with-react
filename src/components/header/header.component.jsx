5f5c82d0e1dc89e40a03d4622d3ed28fc1f6aead
Benaiah Ambie-Barango <bennieb96@gmail.com>
2020-05-01 12:53:17 -0400

    First commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Notes</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
